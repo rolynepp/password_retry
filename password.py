7760f01dcf280cb27475d93465b91c9263c43a71
rolyne chen <rolynepp@gmail.com>
2018-09-05 10:30:33 +0800

    while x>0

--- password.py
+++ password.py
@@ -1,10 +1,11 @@
-while True:
+x = 3
+while x > 0:
 	password = input('請輸入密碼: ')
-	x = 3
-	if 0 < x <= 3:
+	if x > 0:
+		x = x - 1
 		if password == 'a123456':
 			print('登入成功')
 			break
 		else:
-			print('密碼錯誤!還有', x-1, '次機會')
-			x = x - 1
+			print('密碼錯誤!還有', x, '次機會')
+			
